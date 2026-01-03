bedd3396426c7f41a5079d63ad4a60617e4d0e35
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2026-01-02 23:42:57 -0300

    feat: refactor upload widget component and enhance CSS utilities for animated borders

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -8,27 +8,26 @@ import UploadWidgetUploadList from "./upload-widget-upload-list";
 export default function UploadWidget() {
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
   return (
-    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
-      {!isWidgetOpen && <UploadWidgetMinimizeButton />}
-
+    <Collapsible.Root asChild onOpenChange={() => toggleWidgetOpen()}>
       <motion.div
+        data-progress={!isWidgetOpen}
         animate={isWidgetOpen ? "open" : "closed"}
         variants={{
-          closed: {
-            width: "max-content",
-            height: 44,
-            transition: {
-              type: "inertia",
-            },
-          },
-          open: {
-            width: 360,
-            height: "auto",
-            transition: { duration: 0.1 },
-          },
+          closed: { width: "max-content", height: 44 },
+          open: { width: 360, height: "auto" },
         }}
-        className="bg-zinc-900  overflow-hidden shadow-shape max-w-90 rounded-xl"
+        className="
+  bg-zinc-900 overflow-hidden
+  data-[state=open]:shadow-shape
+  data-[state=closed]:rounded-3xl!
+  data-[state=open]:rounded-xl
+  data-[state=closed]:data-[progress=true]:animated-border
+  data-[state=closed]:data-[progress=true]:animated-border-spin
+  max-w-90
+"
       >
+        {!isWidgetOpen && <UploadWidgetMinimizeButton />}
+
         <Collapsible.Content>
           <UploadWidgetHeader />
           <div className="flex flex-col gap-4 py-3">
